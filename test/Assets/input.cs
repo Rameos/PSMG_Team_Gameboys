f174a606bea5659b3a40e0e7cc6f0930f920b672
IrisF <iris.figalist@stud.uni-regensburg.de>
2014-05-13 12:53:14 +0200

    kill script

---
+++ test/Assets/input.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class input : MonoBehaviour {
+
+	public float speed = 0.1f;
+	public float JumpSpeed = 5.0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				float transformV = UnityEngine.Input.GetAxis ("Vertical") * speed;
+				float transformH = UnityEngine.Input.GetAxis ("Horizontal") * speed;
+
+				transform.Translate (transformH, 0, transformV);
+
+				if (Input.GetKeyDown("space")) {
+					Debug.Log("jump");
+			rigidbody.velocity += (Vector3.up *JumpSpeed);
+	
+				}
+		}
+}
+
---
+++ test/Assets/killObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class killObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider Cube){
+		if (Input.GetKeyDown ("k")) {
+			Debug.Log ("kill");
+			Destroy(gameObject);
+		}
+
+	
+	}
+}
