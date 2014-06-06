1da57391fd7d4051d4eb397a8663befd21f6f056
myossinc <wagner.maximilian@outlook.com>
2014-06-06 13:30:21 +0200

    money management on death

--- PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/death.cs
+++ PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/death.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class death : MonoBehaviour {
 
+    // Instance variables
     public Transform player;
+    GameObject playerObject;
+    MoneyManagement money;
 
+    // Initialize objects/scripts
+    void Awake()
+    {
+        playerObject = GameObject.Find("Player");
+        money = playerObject.GetComponent<MoneyManagement>();
+    }
+
+    // Reaction on players death
     void OnTriggerEnter(Collider col)
     {        
         if (col.tag == "Player")
         {
+            // Relocate player in front of canyon on death
             player.position = new Vector3(1149f, 105f, 375f);
+
+            // Subtract money from the players account on death
+            money.subtractMoney(15);
         }
     }
 }
