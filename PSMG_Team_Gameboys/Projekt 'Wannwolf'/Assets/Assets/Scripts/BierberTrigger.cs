e1cf68f865cc90d0960a666d6542d3c7b60ead1f
JonasMa <Jonas.Mattes@stud.uni-regensburg.de>
2014-06-12 22:59:36 +0100

    bierber aufgabe verbesert

--- PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Assets/Scripts/BierberTrigger.cs
+++ PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Assets/Scripts/BierberTrigger.cs
@@ -4,7 +4,12 @@ using System.Collections;
 public class BierberTrigger : MonoBehaviour {
 	
 	void OnTriggerEnter (Collider other) {
-		if(GameObject.Find("bier").transform.parent == GameObject.Find("pickto").transform)
+		if(GameObject.Find("BierberBody").renderer.enabled == false)
+		{
+			GameObject.Find("BierberBody").renderer.enabled = true;
+			GameObject.Find("BierberHead").renderer.enabled = true;
+		}
+		else if(GameObject.Find("bier").transform.parent == GameObject.Find("pickto").transform)
 		{
 			GameObject.Find("bier").transform.parent = null;
 			GameObject.Find("FallenTree").renderer.enabled = true;
