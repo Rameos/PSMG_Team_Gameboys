30efec910af5904377d15f749aa0bcf81967eb54
IrisF <iris.figalist@stud.uni-regensburg.de>
2014-07-06 01:41:26 +0200

    Eyetracker-Kalibrierung im Spiel

---
+++ PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Calibration.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using iViewX;
+
+public class Calibration : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width * 0.5f, Screen.height * 0.0f, Screen.width * 0.1f, Screen.height * 0.1f), "Start Calibration"))
+            GazeControlComponent.Instance.StartCalibration();
+    }
+}
