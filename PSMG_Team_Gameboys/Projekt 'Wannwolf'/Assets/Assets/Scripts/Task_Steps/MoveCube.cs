aef4c0936c6bed7c97e7c3603b602652536aecaf
IrisF <iris.figalist@stud.uni-regensburg.de>
2014-07-29 16:03:39 +0200

    2 objekte lassen sich bewegen, aber noch nicht aufeinander stapeln

--- PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Assets/Scripts/Task_Steps/MoveCube.cs
+++ PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Assets/Scripts/Task_Steps/MoveCube.cs
@@ -5,21 +5,47 @@ public class MoveCube : MonoBehaviour {
 
     public Transform target;
     float moveSpeed = 20;
+    bool moving = false;
+    float stop = 5f;
+    float rotationSpeed = 5;
 
-
+    void Start()
+    {
+        gameObject.GetComponent<FollowPlayer>().enabled = false;
+    }
 
     void OnTriggerStay(Collider other)
     {
 
 
-       // if (Input.GetKeyDown("f"))
-       // {
-           // if (target != null)
-           // {
-                //transform.position = Vector3.MoveTowards(transform.position, target.position, 1);
-                //gameObject.transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
-        transform.position += transform.forward * moveSpeed * Time.deltaTime;
-           // }
+        if (Input.GetKeyDown("f"))
+        {
+            if (moving == false)
+            {
+                moving = true;
+                gameObject.GetComponent<FollowPlayer>().enabled = true;
+            }
+            else
+            {
+                moving = false;
+                gameObject.GetComponent<FollowPlayer>().enabled = false;
+            }
+           }
+        if (moving == true)
+        {
+            //float distance = Vector3.Distance(transform.position,target.position);
+            //if (distance <= stop)
+            //{
+              //  transform.rotation = Quaternion.Slerp(transform.rotation,
+               // Quaternion.LookRotation(target.position - transform.position), rotationSpeed * Time.deltaTime);
+                //transform.Rotate(0, 0, 0);
+            //}
+            //else
+            //{
+              //  Vector3 newPos = target.position;
+                //gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, newPos, 0.2f);
+            //}
+        }
            
       //  }
 
