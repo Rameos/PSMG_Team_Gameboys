38c3d893307a74afbda8a0a1edc87a2fab237387
IrisF <iris.figalist@stud.uni-regensburg.de>
2014-08-04 14:09:51 +0200

    Baumstuempfe bewegen

--- PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Assets/Scripts/Task_Steps/MoveCube.cs
+++ PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Assets/Scripts/Task_Steps/MoveCube.cs
@@ -3,14 +3,17 @@ using System.Collections;
 
 public class MoveCube : MonoBehaviour {
 
-    public Transform target;
-    float moveSpeed = 20;
+ 
+    private bool waitActive = false;
+
+
 
 
 
     void OnTriggerStay(Collider other)
     {
-
+     
+       // id = obj.ToCharArray();
 
        // if (Input.GetKeyDown("f"))
        // {
@@ -18,17 +21,50 @@ public class MoveCube : MonoBehaviour {
            // {
                 //transform.position = Vector3.MoveTowards(transform.position, target.position, 1);
                 //gameObject.transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
-        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        //transform.position += transform.forward * moveSpeed * Time.deltaTime;
            // }
            
       //  }
+        if (gameObject.tag == "m")
+        {
+            if (Input.GetKeyDown("m"))
+            {
+                follow();
+            }
+        }
+        else if (gameObject.tag == "n")
+        {
+            if (Input.GetKeyDown("n"))
+            {
+                follow();
+            }
+        }
 
+    
+    }
 
-
-
-
-
-
+    void follow()
+    {
+        if (gameObject.transform.parent == null && !waitActive)
+        {
+            Debug.Log("if");
+            gameObject.transform.parent = GameObject.Find("pickto").transform;
+            //Destroy(GameObject.Find("Cube").GetComponent("RotateObject"));
+            StartCoroutine(Wait());
+        }
+        else if (!waitActive)
+        {
+            Debug.Log("else");
+            gameObject.transform.parent = null;
+            //GameObject.Find("Cube").AddComponent("RotateObject");
+            StartCoroutine(Wait());
+        }
+    }
+    IEnumerator Wait()
+    {
+        waitActive = true;
+        yield return new WaitForSeconds(0.2f);
+        waitActive = false;
     }
   
 
