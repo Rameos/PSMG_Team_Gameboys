4beb8b6582aa63903e3bfb776aef794a4ab754e1
IrisF <iris.figalist@stud.uni-regensburg.de>
2014-08-04 11:56:08 +0200

    Feuer loeschen

---
+++ PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Assets/Scripts/Task_Fire/PeeOnFire.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using iViewX;
+
+public class PeeOnFire : MonoBehaviourWithGazeComponent
+{
+
+    private bool peeing = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void OnGazeEnter(RaycastHit hit)
+    {
+
+        peeing = true;
+        StartCoroutine(pee(2));
+
+
+    }
+
+    IEnumerator pee(float seconds)
+    {
+
+
+        yield return new WaitForSeconds(seconds);
+        if (peeing == true)
+        {
+
+            Destroy(gameObject); 
+        }
+
+
+
+
+
+    }
+    public override void OnGazeStay(RaycastHit hit)
+    {
+
+      
+
+
+    }
+
+    public override void OnGazeExit()
+    {
+
+        peeing = false;
+
+    }
+}
