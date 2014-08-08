3862a1f407db36cd1f1445fd67f76c939d58e5cc
BRIAS <matthias.braeuer@arcor.de>
2014-08-08 15:32:46 +0200

    Connected fire logic with Eyetracker

--- PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Assets/Scripts/Task_Fire/PeeOnFire.cs
+++ PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Assets/Scripts/Task_Fire/PeeOnFire.cs
@@ -6,53 +6,56 @@ public class PeeOnFire : MonoBehaviourWithGazeComponent
 {
 
     private bool peeing = false;
+    private ParticleSystem fire;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+	fire = gameObject.GetComponent<ParticleSystem>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        shrinkFire();
+        growFire();
 	}
+
+    void shrinkFire()
+    {
+        if (peeing)
+        {
+            if (fire.startLifetime <= 0.1)
+            {
+                GameObject.FindGameObjectWithTag("PlayerFlames").GetComponent<ParticleSystem>().Stop();
+                GameObject.Destroy(fire);
+            }
+            else
+            {
+                fire.startLifetime -= 2 * Time.deltaTime;
+            }
+        }
+    }
+
+    void growFire()
+    {
+        if (!peeing)
+        {
+            if(fire.startLifetime < 4.5f)
+            fire.startLifetime += Time.deltaTime;
+        }
+    }
 
     public override void OnGazeEnter(RaycastHit hit)
     {
         peeing = true;
-        StartCoroutine(pee(2));
-
-
     }
 
-    IEnumerator pee(float seconds)
-    {
-
-
-        yield return new WaitForSeconds(seconds);
-        if (peeing == true)
-        {
-
-            Destroy(gameObject); 
-        }
-
-
-
-
-
-    }
     public override void OnGazeStay(RaycastHit hit)
     {
-
-      
-
 
     }
 
     public override void OnGazeExit()
     {
-
         peeing = false;
-
     }
 }
