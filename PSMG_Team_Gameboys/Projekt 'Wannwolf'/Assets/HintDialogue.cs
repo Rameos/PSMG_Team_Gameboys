a9ea3d203a20b5b783b958e83f10872eaeacb82c
ollpie <oliver.pieper@stud.uni-regensburg.de>
2014-08-11 22:22:59 +0200

    gialoge einsetzen fertig

--- PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/HintDialogue.cs
+++ PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/HintDialogue.cs
@@ -16,68 +16,74 @@ public class HintDialogue : MonoBehaviour {
     const int CHASE_HINT = 10;
     const int ANGLER_AWAKE_HINT = 11;
 
-    public AudioSource myAudio;
-    public AudioClip pizza;
+	bool pizza = false;
+	bool bierber = false;
+	bool sneek = false;
+	bool retrieve = false;
+	bool vodka = false;
+	bool stair = false;
+	bool edge = false;
+	bool tree = false;
+	bool chase = false;
+	bool anglerAwake = false;
+
+	int fireArrival = 0;
+	int vodkaArrival = 0;
 
     public AudioClip[] Dialogues;
-    GameObject player;
-    bool anglerArrival = false;
-    int fireArrival = 0;
-    public static HintDialogue hint;
-
+    
     void Awake()
     {
-        player = GameObject.FindGameObjectWithTag("Player"); 
+        
     }
 
 	// Use this for initialization
 	void Start () {
-        myAudio = gameObject.AddComponent<AudioSource>();
-        myAudio.playOnAwake = false;
+        
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     void OnTriggerEnter(Collider col)
     {
         string tag = gameObject.tag;
         switch (tag)
         {
-            case "PizzaHint": audio.clip = (AudioClip)Resources.Load("pizza");
-                Debug.Log("Jetzt müsste Sound spielen");
-                myAudio.Play();
+			case "PizzaHint": controllPlaytime(pizza, Dialogues[PIZZA_HINT]);
+				pizza = true;
                 break;
-            case "BierberHint": audio.clip = Dialogues[BIERBER_HINT];
-                audio.Play();
-                break;
-            case "AnglerArrivalHint":
-                if (!anglerArrival)
-                {
-                    audio.clip = Dialogues[SNEEK_HINT];
-                    audio.Play();
-                    anglerArrival = true;
-                }
+			case "BierberHint": controllPlaytime(bierber, Dialogues[BIERBER_HINT]);
+				bierber = true;    
+				break;
+        	case "AnglerArrivalHint": controllPlaytime(sneek, Dialogues[SNEEK_HINT]);
+				sneek = true;
                 break;
             case "FireArrivalHint":
                 checkFireArrivalTimes();
                 break;
-            case "JumpHint":
-                audio.clip = Dialogues[JUMP_HINT];
-                audio.Play();
+			case "VodkaHint": 
+				if(vodkaArrival == 2){
+					controllPlaytime(vodka, Dialogues[VODKA_HINT]);
+				}
+				vodkaArrival++;
+				break;
+			case "JumpHint": controllPlaytime(stair, Dialogues[JUMP_HINT]);
+				stair = true;
                 break;
-            case "StairBuildHint":
-                audio.clip = Dialogues[STAIR_HINT];
-                audio.Play();
+			case "StairBuildHint": controllPlaytime(edge, Dialogues[STAIR_HINT]);
+				edge = true;
                 break;
-            case "ChaseHint":
-                audio.clip = Dialogues[BAUM_HINT];
-                audio.Play();
+			case "ChaseHint": controllPlaytime(chase, Dialogues[CHASE_HINT]);
+				chase = true;
                 break;
         }
     }
+
+	void controllPlaytime (bool state, AudioClip sound){
+		if (!state)
+		{
+			audio.clip = sound;
+			audio.Play();
+		}
+	}
 
     void checkFireArrivalTimes()
     {
