7e4934405969b55cb99976fc6f10fe2320b9ed99
myossinc <Maximilian1.Wagner@stud.uni-regensburg.de>
2014-08-19 21:15:30 +0200

    Water adjusted, scale money text
    Water: animation improved
    money text: font size scales with screen resolution

---
+++ PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/ScaleGUIFont.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleGUIFont : MonoBehaviour {
+
+    public int ratio;
+    public GUIText text;
+
+    void Update()
+    {
+        text.fontSize = Mathf.Min(Screen.width, Screen.height) / ratio;
+    }
+}
