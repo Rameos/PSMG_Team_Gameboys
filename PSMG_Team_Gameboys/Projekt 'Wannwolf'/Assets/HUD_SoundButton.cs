5d902ab3cf35e3bd8c422e4a91553f38acbd1ed0
myossinc <Maximilian1.Wagner@stud.uni-regensburg.de>
2014-09-26 16:35:04 +0200

    Add En-/Disable sound button to hud

---
+++ PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/HUD_SoundButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD_SoundButton : MonoBehaviour {
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 130, Screen.height/60, Screen.width / 40, Screen.height / 18), ""))
+        {
+            // Mute all ingame sounds
+            if (AudioListener.pause)
+            {
+                AudioListener.pause = false;
+            }
+            else
+            {
+                AudioListener.pause = true;
+            }
+        }
+    }
+}
