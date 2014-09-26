fe2ad2523790c3de10d04167beb52cbdb2ca9cb2
JonasMa <jonas.mattes@stud.uni-regensburg.de>
2014-09-26 10:40:05 +0200

    beer trigger fixed

--- PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Assets/Scripts/Task_Bierber/BierberTrigger.cs
+++ PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Assets/Scripts/Task_Bierber/BierberTrigger.cs
@@ -16,11 +16,11 @@ public class BierberTrigger : MonoBehaviour {
 		}
 		if(GameObject.FindGameObjectWithTag(TagManager.BEER).transform.parent == GameObject.Find("pickto").transform)
 		{
-			//GameObject.FindGameObjectWithTag(TagManager.BEER).transform.parent = GameObject.Find(TagManager.BIERBER_BODY).transform;
+			GameObject.FindGameObjectWithTag(TagManager.BEER).transform.parent = GameObject.Find(TagManager.BIERBER_BODY).transform;
             GameObject.FindGameObjectWithTag(TagManager.BEER).transform.position = new Vector3(574.5643f, 104.2155f, 233.2953f);
+            Destroy(GameObject.FindGameObjectWithTag(TagManager.BEER).transform.collider);
 			GameObject.FindGameObjectWithTag (TagManager.BIERBER).animation.CrossFade ("BierberWalkAndCutTree", 0f);
 			GameObject.FindGameObjectWithTag(TagManager.BIERBER_INVISIBLE_WALL).collider.enabled = false;
-			//System.Threading.Thread.Sleep (2500);
 			StartCoroutine(fallingTree());
 
 			// Disable the renderer of the angler's minimap icon
