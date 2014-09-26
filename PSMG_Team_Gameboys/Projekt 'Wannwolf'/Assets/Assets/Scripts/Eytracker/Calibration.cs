03e9ab272df509db1506b1d101b45a7a292314a4
IrisF <iris.figalist@stud.uni-regensburg.de>
2014-09-26 11:15:28 +0200

    Kalibrierung bei Start

--- PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Assets/Scripts/Eytracker/Calibration.cs
+++ PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Assets/Scripts/Eytracker/Calibration.cs
@@ -3,11 +3,12 @@ using System.Collections;
 using System.Runtime.InteropServices;
 using iViewX;
 
-public class Calibration : MonoBehaviour {
+public class Calibration : MonoBehaviour
+{
 
-    void OnGUI()
+    public static void calibrate()
     {
-        if (GUI.Button(new Rect(Screen.width * 0.5f, Screen.height * 0.0f, Screen.width * 0.2f, Screen.height * 0.1f), "Start Calibration"))
-            GazeControlComponent.Instance.StartCalibration();
+        Debug.Log("calibrate");
+        GazeControlComponent.Instance.StartCalibration();
     }
 }
--- PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Assets/Scripts/Player/Menu_and_Screens/Menu.cs
+++ PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/Assets/Scripts/Player/Menu_and_Screens/Menu.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class Menu : MonoBehaviour {
 
+    private bool isCalibrated = false;
  
+
 	void OnGUI(){
         if (GUI.Button(new Rect(550, 115, 200, 100), "New Game"))
         {
+            startCalibration();
             LoadScene.loadFirstLevel();
         }
 
@@ -14,12 +17,26 @@ public class Menu : MonoBehaviour {
         {
             if (PlayerPrefs.HasKey("GameSaved"))
             {
+                startCalibration();
                 LoadScene.loadLastGame();
             }
         }
         if (GUI.Button(new Rect(550, 345, 200, 100), "Quit Game"))
         {
             Application.Quit();
+        }
+    }
+
+    void startCalibration()
+    {
+        if (!isCalibrated)
+        {
+            if (gazeModel.posGazeRight.x != 0 && gazeModel.posGazeRight.y != 0)
+            {
+                isCalibrated = true;
+                Calibration.calibrate();
+            }
+
         }
     }
 }
