b6450eca73bbba8730cfa5b59d3376b86af622b8
myossinc <Maximilian1.Wagner@stud.uni-regensburg.de>
2014-09-26 16:51:04 +0200

    Add image to sound button

--- PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/HUD_SoundButton.cs
+++ PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/HUD_SoundButton.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class HUD_SoundButton : MonoBehaviour {
 
+
+    public Texture image;
+
     void OnGUI()
     {
-        if (GUI.Button(new Rect(Screen.width / 130, Screen.height/60, Screen.width / 40, Screen.height / 18), ""))
+        if (GUI.Button(new Rect(Screen.width / 130, Screen.height/60, Screen.width / 40, Screen.height / 18), image))
         {
             // Mute all ingame sounds
             if (AudioListener.pause)
