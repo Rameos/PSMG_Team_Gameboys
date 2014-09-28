3f2b750377d92be6a609efc5b40a0571a77b86ac
myossinc <Maximilian1.Wagner@stud.uni-regensburg.de>
2014-09-28 15:15:46 +0200

    Add Delayed Rendering Script
    Vll kann man damit das Problem lösen, dass man partikelsysteme durch
    ungerenderte bäume sehen kann

---
+++ PSMG_Team_Gameboys/Projekt 'Wannwolf'/Assets/DelayedRendering.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayedRendering : MonoBehaviour {
+
+	// Render this object with a delay
+	void Start () {
+	    renderer.material.renderQueue = 2800; 
+	}
+	
+
+}
